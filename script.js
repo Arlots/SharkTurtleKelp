e17a78ed5ff65647e5bead952968ddd2ca04c35b
Arlots <ethan.jennings092@gmail.com>
2022-08-26 16:10:22 +0900

    Debugged and lightly commented the functions. Final iteration finished.

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ let computerChoice;
 let playerWins = 0;
 let computerWins = 0;
 let resetButton;
+
+const choiceShark = document.querySelector(".sharkLink");
+const choiceTurtle = document.querySelector(".turtleLink");
+const choiceKelp = document.querySelector('.kelpLink');
+
+
+// adds click events to elements on the page.
+choiceShark.addEventListener('click', sharkGame);
+choiceTurtle.addEventListener('click', turtleGame);
+choiceKelp.addEventListener('click', kelpGame);
+
+/* This function uses the math function to get a random number for the computer. Based on the number it chooses an option of shark, turtle or kelp.
+    It also generates an image of the computer's choice into the "Computer Choice" div.
+*/
 function computerCardChoice () {
         number = Math.floor(Math.random() * 3) + 1;
         console.log(`${number}`);
@@ -21,7 +35,7 @@ function computerCardChoice () {
             computerCard.setAttribute("class", "compSharkChoice");
             document.querySelector('.computerCard').appendChild(computerCard);
         } else if (number === 2) {
-            computerChoice = "Turtle"
+            computerChoice = "Turtle";
             if (document.contains(document.querySelector(".compSharkChoice"))) {
                 document.querySelector(".compSharkChoice").remove();
             }   else if (document.contains(document.querySelector(".compTurtleChoice"))){
@@ -34,7 +48,7 @@ function computerCardChoice () {
             computerCard.setAttribute("class", "compTurtleChoice");
             document.querySelector('.computerCard').appendChild(computerCard);
         } else if (number === 3) {
-            computerChoice = "Kelp"
+            computerChoice = "Kelp";
             if (document.contains(document.querySelector(".compSharkChoice"))) {
                 document.querySelector(".compSharkChoice").remove();
             }   else if (document.contains(document.querySelector(".compTurtleChoice"))){
@@ -50,6 +64,7 @@ function computerCardChoice () {
         return computerChoice;
 }
 
+// This function sets the player's choice to "shark" and moves the corresponding image to the "player's choice div"
 function sharkCardChoice() {
     if (document.contains(document.querySelector(".sharkImgChoice"))) {
         document.querySelector(".sharkImgChoice").remove();
@@ -68,6 +83,7 @@ function sharkCardChoice() {
 
 }
 
+// This function sets the player's choice to "turtle" and moves the corresponding image to the "player's choice div"
 function turtleCardChoice() {
     if (document.contains(document.querySelector(".sharkImgChoice"))) {
         document.querySelector(".sharkImgChoice").remove();
@@ -84,7 +100,9 @@ function turtleCardChoice() {
     return playerChoice;
  }
 
- function kelpCardChoice() {
+// This function sets the player's choice to "kelp" and moves the corresponding image to the "player's choice div"
+
+function kelpCardChoice() {
     if (document.contains(document.querySelector(".sharkImgChoice"))) {
         document.querySelector(".sharkImgChoice").remove();
     }   else if (document.contains(document.querySelector(".turtleImgChoice"))){
@@ -100,7 +118,7 @@ function turtleCardChoice() {
     return playerChoice;
  }
 
-
+//Compares the computer and player's choice and determines a winer. It then adjusts the corresponding counter and display a win/lose message for the round.
  function playRound (playerChoice, computerChoice) {
     if ( computerChoice === playerChoice) {
         if (document.contains(document.querySelector(".resultTie"))) {
@@ -263,6 +281,8 @@ function turtleCardChoice() {
      
     }
 
+// runs when a player reachers 5 wins. It prints an overall win/lose message on the screen. It also hides choices so the player can not 
+// continue and displays a reset button for the player to reset the game.
     function gameOver (playerWins, computerWins) {
         if (playerWins === 5) {
             if (document.contains(document.querySelector(".resultTie"))) {
@@ -283,7 +303,11 @@ function turtleCardChoice() {
                 resetButton.setAttribute("class", "resetButton");
                 document.querySelector('.result').appendChild(resetButton);
                 const reset = document.querySelector('.resetButton');
-                reset.addEventListener('click', resetGame);  
+                reset.addEventListener('click', resetGame);
+                choiceShark.removeEventListener('click', sharkGame);
+                choiceTurtle.removeEventListener('click', turtleGame);
+                choiceKelp.removeEventListener('click', kelpGame);
+                document.querySelector('.cardChoices').style.visibility = "hidden";
                 return resetButton; 
 
         } else if (computerWins === 5 ) {
@@ -306,10 +330,16 @@ function turtleCardChoice() {
                     document.querySelector('.result').appendChild(resetButton); 
                     const reset = document.querySelector('.resetButton');
                     reset.addEventListener('click', resetGame);  
+                    choiceShark.removeEventListener('click', sharkGame);
+                    choiceTurtle.removeEventListener('click', turtleGame);
+                    choiceKelp.removeEventListener('click', kelpGame);
+                    document.querySelector('.cardChoices').style.visibility = "hidden";
                     return resetButton;
         }
+
     }
 
+// resets win counts and removes/resets any elements necessary for the player to start a fresh game.
     function resetGame() {
         playerWins = 0;
         computerWins = 0;
@@ -321,6 +351,12 @@ function turtleCardChoice() {
         playerWinCount.appendChild(playerWinCountText);
         playerWinCount.setAttribute("class", "playerWinCount");
         document.querySelector('.playerCounter').appendChild(playerWinCount);
+        document.querySelector('.resetButton').remove();
+        if (document.contains(document.querySelector('.grandWin'))){
+            document.querySelector('.grandWin').remove();
+        } else if (document.contains(document.querySelector('.grandLose'))){
+            document.querySelector('.grandLose').remove();
+        }
 
         if (document.contains(document.querySelector('.computerWinCount'))){
             document.querySelector('.computerWinCount').remove();
@@ -330,8 +366,14 @@ function turtleCardChoice() {
         computerWinCount.appendChild(computerWinCountText);
         computerWinCount.setAttribute("class", "computerWinCount");
         document.querySelector('.computerCounter').appendChild(computerWinCount);
+        document.querySelector('.cardChoices').style.visibility = "visible";
+        choiceShark.addEventListener('click', sharkGame);
+        choiceTurtle.addEventListener('click', turtleGame);
+        choiceKelp.addEventListener('click', kelpGame);
+        
         return playerWins, computerWins;
     }
+//Plays the game with the player's selection set to "shark"
     function sharkGame () {
         sharkCardChoice();
         computerCardChoice();
@@ -339,6 +381,7 @@ function turtleCardChoice() {
         gameOver(playerWins, computerWins);
     }
 
+//Plays the game with the player's selection set to "turtle"
     function turtleGame () {
         turtleCardChoice();
         computerCardChoice();
@@ -346,6 +389,7 @@ function turtleCardChoice() {
         gameOver(playerWins, computerWins);
     }
 
+//Plays the game with the player's selection set to "kelp"
     function kelpGame () {
         kelpCardChoice();
         computerCardChoice();
@@ -354,14 +398,7 @@ function turtleCardChoice() {
     }
 
 
-const choiceShark = document.querySelector(".sharkLink");
-const choiceTurtle = document.querySelector(".turtleLink");
-const choiceKelp = document.querySelector('.kelpLink');
 
-
-choiceShark.addEventListener('click', sharkGame);
-choiceTurtle.addEventListener('click', turtleGame);
-choiceKelp.addEventListener('click', kelpGame);
 
 
 
